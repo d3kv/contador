895c9cb0ace3713ef92100bf31123b199bb9dc6a
unknown <adielrocaflores42@gmail.com>
2026-04-24 11:39:58 +0200

    hecho

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+function Home() {
+  let unidades = 0;
+  let decenas = 0;
+  let centenas = 0;
+  let umillar = 0;
+  let dmillar = 0;
+  let cmillar = 0;
+
+  setInterval(() => {
+    unidades++;
+
+    if (unidades > 9) {
+      unidades = 0;
+      decenas++;
+    }
+
+    if (decenas > 9) {
+      decenas = 0;
+      centenas++;
+    }
+
+	if (centenas > 9) {
+      centenas = 0;
+      umillar++;
+    }
+
+
+	if (umillar > 9) {
+      umillar = 0;
+      dmillar++;
+    }
+	
+	if (dmillar > 9) {
+      dmillar = 0;
+      cmillar++;
+    }
+
+
+
+    document.getElementById("u").innerText = unidades;
+    document.getElementById("d").innerText = decenas;
+    document.getElementById("c").innerText = centenas;
+	document.getElementById("um").innerText = umillar;
+	document.getElementById("dm").innerText = dmillar;
+	document.getElementById("cm").innerText = cmillar;
+
+  }, 1000);
+
+  return (
+    <div className="d-flex align-items-center justify-content-center ms-auto text-center " style={{ gap: "10px", fontSize: "40px" }}
+>
+      <div className="border border-grey p-3 bg-dark text-white" id="cm">0</div>
+	  <div className="border border-grey p-3 bg-dark text-white" id="dm">0</div>
+	  <div className="border border-grey p-3 bg-dark text-white" id="um">0</div>
+	  <div className="border border-grey p-3 bg-dark text-white" id="c">0</div>
+      <div className="border border-grey p-3 bg-dark text-white" id="d">0</div>
+      <div className="border border-grey p-3 bg-dark text-white" id="u">0</div>
+    </div>
+  );
+}
 
 export default Home;
